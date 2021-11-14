b9b175f8aaae1cf695f40b662bcacc7d44ce7c30
Tanaka Chitete <tanaka.chitete@gmail.com>
2021-11-14 23:13:41 +0800

    integrate FlatLaf

--- app/src/main/java/amethyst/main/App.java
+++ app/src/main/java/amethyst/main/App.java
@@ -2,9 +2,12 @@ package amethyst.main;
 
 import amethyst.view.Amethyst;
 import amethyst.controller.Controller;
+import com.formdev.flatlaf.FlatDarkLaf;
 
 public class App {
     public static void main(String[] args) {
+        FlatDarkLaf.setup();
+
         new Controller(new Amethyst());
     }
 }
